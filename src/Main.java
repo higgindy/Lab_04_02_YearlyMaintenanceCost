a04e03e49fc7c09c4efc57c1c9ba81772ba1fb90
Dylan Higgins <higgindy@mail.uc.edu>
2023-09-20 16:38:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int summerCost = 30;
+        int winterCost = 50;
+        int fallCost = 20;
+        int springCost = 40;
+        int yearTotal = 0;
+
+        yearTotal = summerCost + winterCost + fallCost + springCost;
+        System.out.println("The maintenance cost for each season is as follows: summer: " + summerCost + " winter: " + winterCost + " fall: " + fallCost + " spring: " + springCost +
+                " and the yearly maintenance cost is: " + yearTotal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
